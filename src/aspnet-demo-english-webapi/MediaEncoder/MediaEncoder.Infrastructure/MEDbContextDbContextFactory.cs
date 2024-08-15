e98f9ae3370875e6779c583d9e9958fdb32820f1
zrq <wyzrq163@163.com>
2024-08-15 21:02:22 +0800

    [MediaEncoder] MediaEncoder代码迁移 以及生成数据库

---
+++ src/aspnet-demo-english-webapi/MediaEncoder/MediaEncoder.Infrastructure/MEDbContextDbContextFactory.cs
@@ -0,0 +1,15 @@
+﻿using CommonInitializer;
+using MediaEncoder.Infrastructure;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace Listening.Infrastructure
+{
+    public class MEDbContextDbContextFactory : IDesignTimeDbContextFactory<MEDbContext>
+    {
+        public MEDbContext CreateDbContext(string[] args) 
+        {
+            var dbOptions = DbContextOptionsBuilderFactory.Create<MEDbContext>();
+            return new MEDbContext(dbOptions.Options,null);
+        }
+    }
+}
