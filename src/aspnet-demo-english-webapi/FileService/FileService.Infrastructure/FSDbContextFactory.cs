cbc9a44626f390a8b4d62abc4237d887f953ea88
zrq <wyzrq163@163.com>
2024-08-15 10:10:51 +0800

    [FileServers] 迁移FileServers项目

---
+++ src/aspnet-demo-english-webapi/FileService/FileService.Infrastructure/FSDbContextFactory.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore.Design;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommonInitializer;
+
+namespace FileService.Infrastructure
+{
+    public class FSDbContextFactory : IDesignTimeDbContextFactory<FSDbContext>
+    {
+        public FSDbContext CreateDbContext(string[] args) 
+        {
+            var dbOptions = DbContextOptionsBuilderFactory.Create<FSDbContext>();
+            return new FSDbContext(dbOptions.Options, null);
+        }
+    }
+
+}
