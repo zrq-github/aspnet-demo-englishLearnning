9b2f44b6aa85b018f375991404ec5412e3cb084c
zrq <wyzrq163@163.com>
2024-08-15 19:09:31 +0800

    [Listening] Listening代码迁移以及生成数据库

---
+++ src/aspnet-demo-english-webapi/Listening/Listening.Infrastructure/IdDbContextFactory.cs
@@ -0,0 +1,15 @@
+﻿using CommonInitializer;
+using Listening.Domain;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace Listening.Infrastructure
+{
+    public class IdDbContextFactory : IDesignTimeDbContextFactory<ListeningDbContext>
+    {
+        public ListeningDbContext CreateDbContext(string[] args) 
+        {
+            var dbOptions = DbContextOptionsBuilderFactory.Create<ListeningDbContext>();
+            return new ListeningDbContext(dbOptions.Options,null);
+        }
+    }
+}
---
+++ src/aspnet-demo-english-webapi/Listening/Listening.Infrastructure/ModuleInitializer.cs
@@ -0,0 +1,14 @@
+﻿using Listening.Infrastructure;
+using Microsoft.Extensions.DependencyInjection;
+using Zack.Commons;
+
+namespace Listening.Domain
+{
+    class ModuleInitializer : IModuleInitializer
+    {
+        public void Initialize(IServiceCollection services)
+        {
+            services.AddScoped<IListeningRepository, ListeningRepository>();
+        }
+    }
+}
