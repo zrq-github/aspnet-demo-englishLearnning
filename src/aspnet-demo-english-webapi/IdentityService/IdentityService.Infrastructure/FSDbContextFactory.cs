394181dad3a0013bc13d62421e3387bf3bb466a7
zrq <wyzrq163@163.com>
2024-08-15 19:00:21 +0800

    [IdentityService] 代码迁移以及生成数据库

---
+++ src/aspnet-demo-english-webapi/IdentityService/IdentityService.Infrastructure/FSDbContextFactory.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore.Design;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommonInitializer;
+using IdentityService.Infrastructure;
+
+namespace FileService.Infrastructure
+{
+    public class IdDbContextFactory : IDesignTimeDbContextFactory<IdDbContext>
+    {
+        public IdDbContext CreateDbContext(string[] args) 
+        {
+            var dbOptions = DbContextOptionsBuilderFactory.Create<IdDbContext>();
+            return new IdDbContext(dbOptions.Options);
+        }
+    }
+
+}
