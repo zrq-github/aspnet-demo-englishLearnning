a558330bf9c7673d81681479dc20563953582f38
zrq <wyzrq163@163.com>
2024-09-24 21:24:12 +0800

    WebAPI风格对比

---
+++ test-demo/WebAPI风格/WebAPI风格/RPCStyle.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI风格
+{
+    public record User(int Id, string Name);
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RPCStyle : ControllerBase
+    {
+        // POST: api/User/CreateUser
+        [HttpPost("CreateUser")]
+        public IActionResult CreateUser([FromBody] User user)
+        {
+            // 执行创建用户操作
+            return Ok(new { message = "User created successfully" });
+        }
+
+        // POST: api/User/DeleteUser
+        [HttpPost("DeleteUser")]
+        public IActionResult DeleteUser([FromBody] int userId)
+        {
+            // 执行删除用户操作
+            return Ok(new { message = "User deleted successfully" });
+        }
+
+        // POST: api/User/GetUserInfo
+        [HttpPost("GetUserInfo")]
+        public IActionResult GetUserInfo([FromBody] int userId)
+        {
+            // 获取用户信息
+            var user = new User(userId, "张三");
+            return Ok(user);
+        }
+    }
+}
---
+++ test-demo/WebAPI风格/WebAPI风格/UsersController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI风格
+{
+    /// <summary>
+    /// Rest风格
+    /// </summary>
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        // GET: api/Users/{id}
+        [HttpGet("{id}")]
+        public IActionResult GetUser(int id)
+        {
+            // 获取用户信息
+            var user = new User(id, "张三");
+            return Ok(user);
+        }
+
+        // POST: api/Users
+        [HttpPost]
+        public IActionResult CreateUser([FromBody] User user)
+        {
+            // 创建用户
+            return CreatedAtAction(nameof(GetUser), new { id = user.Id }, user);
+        }
+
+        // PUT: api/Users/{id}
+        [HttpPut("{id}")]
+        public IActionResult UpdateUser(int id, [FromBody] User user)
+        {
+            // 更新用户
+            //user.Id = id;
+            return Ok(user);
+        }
+
+        // DELETE: api/Users/{id}
+        [HttpDelete("{id}")]
+        public IActionResult DeleteUser(int id)
+        {
+            // 删除用户
+            return NoContent();
+        }
+    }
+}
