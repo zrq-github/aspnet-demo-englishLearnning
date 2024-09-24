6b2afe91d8f042b5796d2dcf82df6bf6aaa7af7f
zrq <wyzrq163@163.com>
2024-09-24 10:48:04 +0800

    Nlog

---
+++ test-demo/LogDemo/LogDemo/LogDemo/LogTestClass.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogDemo
+{
+    internal class LogTestClass(ILogger<LogTestClass> logger)
+    {
+        public void ConsoleLog()
+        {
+            logger.LogTrace("log trace...");
+            logger.LogDebug("log debug....");
+            logger.LogInformation("log information...");
+            logger.LogWarning("log warning...");
+            logger.LogError("log error...");
+        }
+    }
+}
