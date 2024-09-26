44c033ca6f125171a550778e6b78888312ac80b8
zrq <wyzrq163@163.com>
2024-09-26 09:39:12 +0800

    中间件流程测试

---
+++ test-demo/中间件流程/TestMiddleware.cs
@@ -0,0 +1,12 @@
+﻿namespace 中间件流程
+{
+    public class TestMiddleware:IMiddleware
+    {
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            await context.Response.WriteAsync("TestMiddleware Start<br/>");
+            await next.Invoke(context);
+            await context.Response.WriteAsync("TestMiddleware End<br/>");
+        }
+    }
+}
