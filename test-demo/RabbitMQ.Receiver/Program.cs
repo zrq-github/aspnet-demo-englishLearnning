80dab8fffd5efcd22b23e9269cb62a80e7a427ad
zrq <wyzrq163@163.com>
2024-09-26 22:02:16 +0800

    RabbitMQ的基本使用

---
+++ test-demo/RabbitMQ.Receiver/Program.cs
@@ -0,0 +1,63 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace RabbitMQ.Receiver;
+
+internal class Program
+{
+    private static Task Receiver()
+    {
+        return Task.CompletedTask;
+    }
+
+    private static async Task Main(string[] args)
+    {
+        var factory = new ConnectionFactory();
+        // "guest"/"guest" by default, limited to localhost connections
+        factory.UserName = ConnectionFactory.DefaultUser;
+        factory.Password = ConnectionFactory.DefaultPass;
+        factory.VirtualHost = ConnectionFactory.DefaultVHost;
+        factory.HostName = "localhost";
+
+        factory.DispatchConsumersAsync = true;
+
+        using var conn = factory.CreateConnection();
+        using var channel = conn.CreateModel();
+        channel.ExchangeDeclare("direct_logs", ExchangeType.Direct);
+        channel.QueueDeclare("queue_logs", false, false, false, null);
+        channel.QueueBind("queue_logs", "direct_logs", "logs");
+
+        // Passive Declaration - 被动宣言
+        // 如果队列不存在，会抛出错误
+        var response = channel.QueueDeclarePassive("queue_logs");
+        var messageCount = response.MessageCount;   // returns the number of messages in Ready state in the queue
+        var consumerCount = response.ConsumerCount; // returns the number of consumers the queue has
+
+        // 接受消息
+        var consumer = new AsyncEventingBasicConsumer(channel);
+        consumer.Received += (ch, ea) =>
+        {
+            var body = ea.Body.ToArray();
+            var message = Encoding.UTF8.GetString(body);
+            Console.WriteLine($"Receiver accept message: {message}");
+            // copy or deserialise the payload
+            // and process the message
+            // ...
+
+            //channel?.BasicAck(ea.DeliveryTag, false);
+            return Task.CompletedTask;
+        };
+        // this consumer tag identifies the subscription
+        // when it has to be cancelled
+        var consumerTag = channel.BasicConsume("queue_logs", true, consumer);
+        // ensure we get a delivery
+        //bool waitRes = latch.WaitOne(2000);
+
+        Console.WriteLine("按回车退出");
+        Console.ReadLine();
+
+        channel.Close();
+        conn.Close();
+    }
+}
---
+++ test-demo/RabbitMQ.Sender/Program.cs
@@ -0,0 +1,59 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+namespace RabbitMQ.Sender;
+
+internal class Program
+{
+    private static async Task Sender()
+    {
+        ConnectionFactory factory = new ConnectionFactory();
+        // "guest"/"guest" by default, limited to localhost connections
+        factory.UserName = ConnectionFactory.DefaultUser;
+        factory.Password = ConnectionFactory.DefaultPass;
+        factory.VirtualHost = ConnectionFactory.DefaultVHost;
+        factory.HostName = "localhost";
+
+        using var conn = factory.CreateConnection();
+        using var channel = conn.CreateModel();
+        channel.ExchangeDeclare("direct_logs", ExchangeType.Direct);
+        channel.QueueDeclare("queue_logs", false, false, false, null);
+        channel.QueueBind("queue_logs", "direct_logs", "logs");
+
+        // Passive Declaration - 被动宣言
+        // 如果队列不存在，会抛出错误
+        var response = channel.QueueDeclarePassive("queue_logs");
+        var messageCount = response.MessageCount;   // returns the number of messages in Ready state in the queue
+        var consumerCount = response.ConsumerCount; // returns the number of consumers the queue has
+
+        // 清除队列中的消息
+        var queuePurge = channel.QueuePurge("queue_logs");
+
+        // 创建消息属性
+        IBasicProperties props = channel.CreateBasicProperties();
+        props.ContentType = "text/plain";
+        props.DeliveryMode = 2;        // 设置为持久化模式
+        props.Expiration = "36000000"; // 设置消息过期时间为 10 小时
+
+        // 循环发送消息
+        while (true)
+        {
+            response = channel.QueueDeclarePassive("queue_logs");
+            Console.WriteLine($"messageCount:{response.MessageCount}, consumerCount:{response.ConsumerCount}");
+            string message = $"sender send message: dateTime {DateTime.Now}";
+            channel.BasicPublish("direct_logs", "logs", props, Encoding.UTF8.GetBytes(message));
+            Console.WriteLine();
+            await Task.Delay(1000);
+        }
+
+        channel.Close();
+        conn.Close();
+    }
+
+    private static async Task Main(string[] args)
+    {
+        await Sender();
+
+        Console.ReadKey();
+    }
+}
